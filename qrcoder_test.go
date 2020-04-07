b5c2e35a410fe72f44466871e5c06ec9c16e3d80
jhamPac <cloud@kevia.me>
2020-04-06 19:02:49 -0700

    testing that an error was propagated

--- qrcoder_test.go
+++ qrcoder_test.go
@@ -2,9 +2,16 @@ package qrcoder
 
 import (
 	"bytes"
+	"errors"
 	"image/png"
 	"testing"
 )
+
+type ErrorWriter struct{}
+
+func (e *ErrorWriter) Write(b []byte) (int, error) {
+	return 0, errors.New("Expected error")
+}
 
 func TestGenerateQRCodeReturnsValue(t *testing.T) {
 	buffer := new(bytes.Buffer)
@@ -23,5 +30,14 @@ func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
 
 	if err != nil {
 		t.Errorf("Generated QRcode is not a PNG: %s", err)
+	}
+}
+
+func TestGenerateQRCodePropagatesErrors(t *testing.T) {
+	w := new(ErrorWriter)
+	err := Generate(w, "898r434")
+
+	if err == nil || err.Error() != "Expected error" {
+		t.Errorf("Error did not propagate, got %v:", err)
 	}
 }
