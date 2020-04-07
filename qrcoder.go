30925bff988732b45c9312195c5fc5f1ce66c28e
jhamPac <cloud@kevia.me>
2020-04-06 18:34:03 -0700

    png test passes

--- qrcoder.go
+++ qrcoder.go
@@ -1,6 +1,15 @@
 package qrcoder
 
+import (
+	"bytes"
+	"image"
+	"image/png"
+)
+
 // GenerateQRCode generates a QR code with a given string
 func GenerateQRCode(code string) []byte {
-	return []byte{0xFF}
+	img := image.NewNRGBA(image.Rect(0, 0, 21, 21))
+	buf := new(bytes.Buffer)
+	_ = png.Encode(buf, img)
+	return buf.Bytes()
 }
