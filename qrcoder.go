6949cbdecca16c4eddd72166e663ebd028c1b84c
jhamPac <cloud@kevia.me>
2020-04-06 19:25:58 -0700

    added a third parameter for pattern sizes

--- qrcoder.go
+++ qrcoder.go
@@ -6,8 +6,11 @@ import (
 	"io"
 )
 
+// Version represents module size for QR codes
+type Version int8
+
 // Generate generates a QR code with a given string
-func Generate(w io.Writer, code string) error {
+func Generate(w io.Writer, code string, version Version) error {
 	img := image.NewNRGBA(image.Rect(0, 0, 21, 21))
 	return png.Encode(w, img)
 }
--- qrcoder_test.go
+++ qrcoder_test.go
@@ -16,14 +16,14 @@ func (e *ErrorWriter) Write(b []byte) (int, error) {
 func TestGenerateQRCodeReturnsValue(t *testing.T) {
 	buffer := new(bytes.Buffer)
 
-	if err := Generate(buffer, "555-1234"); err != nil {
+	if err := Generate(buffer, "555-1234", Version(1)); err != nil {
 		t.Errorf("Oops something happened %v:", err)
 	}
 }
 
 func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
 	buffer := new(bytes.Buffer)
-	Generate(buffer, "1234567")
+	Generate(buffer, "1234567", Version(1))
 	_, err := png.Decode(buffer)
 
 	if err != nil {
@@ -33,9 +33,20 @@ func TestGenerateQRCodeGeneratesPNG(t *testing.T) {
 
 func TestGenerateQRCodePropagatesErrors(t *testing.T) {
 	w := new(ErrorWriter)
-	err := Generate(w, "898r434")
+	err := Generate(w, "898r434", Version(1))
 
 	if err == nil || err.Error() != "Expected error" {
 		t.Errorf("Error did not propagate, got %v:", err)
+	}
+}
+
+func TestVersionDeterminesSize(t *testing.T) {
+	buffer := new(bytes.Buffer)
+	_ = Generate(buffer, "some text", Version(1))
+
+	img, _ := png.Decode(buffer)
+
+	if width := img.Bounds().Dx(); width != 21 {
+		t.Errorf("Version 1, expected 21 but got %d", width)
 	}
 }
